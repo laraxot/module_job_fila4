9691cd3d7bb731c64bb2f2efb795fdb97b6a502e
marco76tv <marco.sottana@gmail.com>
2025-10-22 09:29:02 +0200

    .

--- app/Actions/GetTaskCommandsAction.php
+++ app/Actions/GetTaskCommandsAction.php
@@ -4,25 +4,11 @@ declare(strict_types=1);
 
 namespace Modules\Job\Actions;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-use Webmozart\Assert\Assert;
->>>>>>> e1b0bf9 (.)
-=======
->>>>>>> 7d4742a (.)
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Artisan;
 use Spatie\QueueableAction\QueueableAction;
 use Symfony\Component\Console\Command\Command;
-<<<<<<< HEAD
-<<<<<<< HEAD
 use Webmozart\Assert\Assert;
-=======
->>>>>>> e1b0bf9 (.)
-=======
-use Webmozart\Assert\Assert;
->>>>>>> 7d4742a (.)
 
 class GetTaskCommandsAction
 {
@@ -34,10 +20,6 @@ class GetTaskCommandsAction
         $all_commands = collect(Artisan::all());
 
         /*
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7d4742a (.)
          * $command_filter = config('totem.artisan.command_filter');
          * $whitelist = config('totem.artisan.whitelist', true);
          *
@@ -66,37 +48,5 @@ class GetTaskCommandsAction
 
             return $name;
         });
-<<<<<<< HEAD
-=======
-        $command_filter = config('totem.artisan.command_filter');
-        $whitelist = config('totem.artisan.whitelist', true);
-
-        if (! empty($command_filter)) {
-            // $all_commands = $all_commands->filter(function (Command $command) use ($command_filter, $whitelist) {
-            $all_commands = $all_commands->filter(function ($command) use ($command_filter, $whitelist) {
-                foreach ($command_filter as $filter) {
-                    if (fnmatch($filter, $command->getName())) {
-                        return $whitelist;
-                    }
-                }
-
-                return ! $whitelist;
-            });
-        }
-        */
-        return $all_commands->sortBy(
-            static function (Command $command): string {
-                $name = $command->getName();
-                Assert::string($name);
-                if (mb_strpos($name, ':') === false) {
-                    return ':'.$name;
-                }
-
-                return $name;
-            }
-        );
->>>>>>> e1b0bf9 (.)
-=======
->>>>>>> 7d4742a (.)
     }
 }

0a510419b8e463ab23b653520268628f0c5156cf
marco76tv <marco.sottana@gmail.com>
2025-11-11 13:57:39 +0100

    .

--- resources/views/filament/pages/job-monitor.blade.php
+++ resources/views/filament/pages/job-monitor.blade.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 ?>
 <x-filament::page>
-<<<<<<< HEAD
     <livewire:job.status></livewire:job.status>
-=======
-<livewire:job.status></livewire:job.status>
->>>>>>> a51482b (.)
 </x-filament::page>
 <x-filament::page>
 <livewire:job.status></livewire:job.status>

8ab374fa267aa08e20f3450e820d98c8f4249fc7
marco76tv <marco.sottana@gmail.com>
2025-11-12 10:01:22 +0100

    .

--- Config/config.php
+++ Config/config.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+<<<<<<< HEAD
 return [
     'name' => 'Job',
     'description' => 'Modulo per la gestione dei lavori in background e code',
@@ -18,3 +19,8 @@ return [
         'Modules\\Job\\Providers\\JobServiceProvider',
     ],
 ];
+=======
+
+
+return [];
+>>>>>>> a51482b (.)

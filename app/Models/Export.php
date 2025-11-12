e4630d5322f71b129e3327a9bc79a19945a83eba
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:05:44 +0100

    .

--- app/Models/Export.php
+++ app/Models/Export.php
@@ -52,8 +52,6 @@ use Illuminate\Support\Carbon;
  * @method static Builder<static>|Export whereUserId($value)
  * @method static Builder<static>|Export whereUserType($value)
  * @mixin Eloquent
- * @mixin IdeHelperExport
- * @mixin Eloquent
  */
 class Export extends BaseExport
 {

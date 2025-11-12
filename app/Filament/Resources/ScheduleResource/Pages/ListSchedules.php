e43f786309f95eb17eb35977836d329617fc6d04
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:07:52 +0100

    .

--- app/Filament/Resources/ScheduleResource/Pages/ListSchedules.php
+++ app/Filament/Resources/ScheduleResource/Pages/ListSchedules.php
@@ -67,7 +67,7 @@ class ListSchedules extends XotBaseListRecords
             ViewAction::make()
                 ->icon('history')
                 ->color('gray')
-                ->tooltip(__('buttons.history')),
+                ->tooltip(static::trans('buttons.history')),
         ];
     }
 

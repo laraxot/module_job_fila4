fecedd3c08a93b7193198e09c3832a2cefa41440
marco76tv <marco.sottana@gmail.com>
2025-11-12 10:01:27 +0100

    .

--- app/Filament/Resources/FailedJobResource.php
+++ app/Filament/Resources/FailedJobResource.php
@@ -22,21 +22,22 @@ class FailedJobResource extends XotBaseResource
     #[Override]
     public static function getFormSchema(): array
     {
-        return array_values([
-            'uuid' => TextInput::make('uuid')->disabled()->columnSpan(4),
-            'failed_at' => TextInput::make('failed_at')->disabled(),
-            'id' => TextInput::make('id')->disabled(),
-            'connection' => TextInput::make('connection')->disabled(),
-            'queue' => TextInput::make('queue')->disabled(),
-            'exception' => Textarea::make('exception')
+        return [
+            TextInput::make('uuid')->disabled()->columnSpan(4),
+            TextInput::make('failed_at')->disabled(),
+            TextInput::make('id')->disabled(),
+            TextInput::make('connection')->disabled(),
+            TextInput::make('queue')->disabled(),
+            Textarea::make('exception')
                 ->disabled()
                 ->columnSpan(4)
                 ->extraInputAttributes(['style' => 'font-size: 80%;']),
-            'payload' => Textarea::make('payload')
+            Textarea::make('payload')
                 ->disabled()
                 ->columnSpan(4)
                 ->extraInputAttributes(['style' => 'font-size: 80%;']),
-        ]);}
+        ];
+    }
 
     #[Override]
     public static function getRelations(): array

e96fb2770623ead806c67217d9b7080d4b6ae460
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:04:57 +0100

    .

--- rector.php
+++ rector.php
@@ -11,10 +11,6 @@ use RectorLaravel\Rector\MethodCall\RedirectRouteToToRouteHelperRector;
 use RectorLaravel\Set\LaravelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7d4742a (.)
     $rectorConfig->paths([
         __DIR__,
     ]);
@@ -24,34 +20,12 @@ return static function (RectorConfig $rectorConfig): void {
         '*/docs',
         '*/vendor',
     ]);
-<<<<<<< HEAD
-=======
-    $rectorConfig->paths(
-        [
-            __DIR__,
-        ]
-    );
-
-    $rectorConfig->skip(
-        [
-            './vendor/',
-            '*/docs',
-            '*/vendor',
-        ]
-    );
->>>>>>> e1b0bf9 (.)
-=======
->>>>>>> 7d4742a (.)
 
     // register a single rule
     // $rectorConfig->rule(InlineConstructorDefaultToPropertyRector::class);
     $rectorConfig->rule(RedirectRouteToToRouteHelperRector::class);
 
     // define sets of rules
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7d4742a (.)
     $rectorConfig->sets([
         PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
         // SetList::DEAD_CODE,
@@ -65,27 +39,6 @@ return static function (RectorConfig $rectorConfig): void {
         // SetList::EARLY_RETURN,
         // SetList::INSTANCEOF,
     ]);
-<<<<<<< HEAD
-=======
-    $rectorConfig->sets(
-        [
-            PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
-            // SetList::DEAD_CODE,
-            // SetList::CODE_QUALITY,
-            LevelSetList::UP_TO_PHP_81,
-            LaravelSetList::LARAVEL_100,
-
-            // SetList::NAMING, //problemi con injuction
-            SetList::TYPE_DECLARATION,
-            // SetList::CODING_STYLE,
-            // SetList::PRIVATIZATION,//problemi con final
-            // SetList::EARLY_RETURN,
-            // SetList::INSTANCEOF,
-        ]
-    );
->>>>>>> e1b0bf9 (.)
-=======
->>>>>>> 7d4742a (.)
 
     $rectorConfig->importNames();
 };

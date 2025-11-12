d9321e1eee2c3788da3cc1291480878f5523d1cb
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:03:58 +0100

    .

--- resources/views/livewire/schedule/create.blade.php
+++ resources/views/livewire/schedule/create.blade.php
@@ -1,8 +1,11 @@
+<<<<<<< HEAD
 <?php
 
 declare(strict_types=1);
 
 ?>
+=======
+>>>>>>> e1b0bf9 (.)
 <div>
     <form wire:submit.prevent="store">
         <div class="uk-flex uk-flex-between uk-flex-middle">
@@ -11,7 +14,11 @@ declare(strict_types=1);
 
         <div class="uk-grid">
             <div class="uk-width-1-1@s uk-width-1-3@m">
+<<<<<<< HEAD
                 <label class="uk-form-label">Description</label>
+=======
+                <x-filament-forms::field-wrapper.label class="uk-form-label">Description</label>
+>>>>>>> e1b0bf9 (.)
                 <div class="uk-text-meta">Provide a descriptive name for your task</div>
             </div>
             <div class="uk-width-1-1@s uk-width-2-3@m">
@@ -25,7 +32,11 @@ declare(strict_types=1);
         </div>
         <div class="uk-grid">
             <div class="uk-width-1-1@s uk-width-1-3@m">
+<<<<<<< HEAD
                 <label class="uk-form-label">Command</label>
+=======
+                <x-filament-forms::field-wrapper.label class="uk-form-label">Command</label>
+>>>>>>> e1b0bf9 (.)
                 <div class="uk-text-meta">Select an artisan command to schedule</div>
             </div>
             <div class="uk-width-1-1@s uk-width-2-3@m">
@@ -37,7 +48,11 @@ declare(strict_types=1);
         </div>
         <div class="uk-grid">
             <div class="uk-width-1-1@s uk-width-1-3@m">
+<<<<<<< HEAD
                 <label class="uk-form-label">Parameters (Optional)</label>
+=======
+                <x-filament-forms::field-wrapper.label class="uk-form-label">Parameters (Optional)</label>
+>>>>>>> e1b0bf9 (.)
                 <div class="uk-text-meta">Command parameters required to run the selected command</div>
             </div>
             <div class="uk-width-1-1@s uk-width-2-3@m">
@@ -48,7 +63,11 @@ declare(strict_types=1);
         <hr class="uk-divider-icon">
         <div class="uk-grid">
             <div class="uk-width-1-1@s uk-width-1-3@m">
+<<<<<<< HEAD
                 <label class="uk-form-label">Timezone</label>
+=======
+                <x-filament-forms::field-wrapper.label class="uk-form-label">Timezone</label>
+>>>>>>> e1b0bf9 (.)
                 <div class="uk-text-meta">Select a timezone for your task. App timezone is selected by default</div>
             </div>
             <div class="uk-width-1-1@s uk-width-2-3@m">
@@ -70,17 +89,28 @@ declare(strict_types=1);
                         <div class="uk-text-meta">Choose whether to define a cron expression or to add frequencies</div>
                     </div>
                     <div class="uk-width-1-1@s uk-width-2-3@m uk-form-controls-text">
+<<<<<<< HEAD
                         <label>
                             <input type="radio" name="type" v-model="type" value="expression"> Expression
                         </label><br>
                         <label>
+=======
+                        <x-filament-forms::field-wrapper.label>
+                            <input type="radio" name="type" v-model="type" value="expression"> Expression
+                        </label><br>
+                        <x-filament-forms::field-wrapper.label>
+>>>>>>> e1b0bf9 (.)
                             <input type="radio" name="type" v-model="type" value="frequency"> Frequencies
                         </label>
                     </div>
                 </div>
                 <div class="uk-grid" v-if="isCron">
                     <div class="uk-width-1-1@s uk-width-1-3@m">
+<<<<<<< HEAD
                         <label class="uk-form-label">Cron Expression</label>
+=======
+                        <x-filament-forms::field-wrapper.label class="uk-form-label">Cron Expression</label>
+>>>>>>> e1b0bf9 (.)
                         <div class="uk-text-meta">Add a cron expression for your task</div>
                     </div>
                     <div class="uk-width-1-1@s uk-width-2-3@m">
@@ -94,7 +124,11 @@ declare(strict_types=1);
                 </div>
                 <div class="uk-grid" v-if="managesFrequencies">
                     <div class="uk-width-1-1@s uk-width-1-3@m">
+<<<<<<< HEAD
                         <label class="uk-form-label">Frequencies</label>
+=======
+                        <x-filament-forms::field-wrapper.label class="uk-form-label">Frequencies</label>
+>>>>>>> e1b0bf9 (.)
                         <div class="uk-text-meta">Add frequencies to your task. These frequencies will be converted into
                             a cron expression while scheduling the task</div>
                     </div>
@@ -161,7 +195,11 @@ declare(strict_types=1);
         <hr class="uk-divider-icon">
         <div class="uk-grid">
             <div class="uk-width-1-1@s uk-width-1-3@m">
+<<<<<<< HEAD
                 <label class="uk-form-label">Email Notification (optional)</label>
+=======
+                <x-filament-forms::field-wrapper.label class="uk-form-label">Email Notification (optional)</label>
+>>>>>>> e1b0bf9 (.)
                 <div class="uk-text-meta">Add an email address to receive notifications when this task gets executed.
                     Leave empty if you do not wish to receive email notifications</div>
             </div>
@@ -176,7 +214,11 @@ declare(strict_types=1);
         </div>
         <div class="uk-grid">
             <div class="uk-width-1-1@s uk-width-1-3@m">
+<<<<<<< HEAD
                 <label class="uk-form-label">SMS Notification (optional)</label>
+=======
+                <x-filament-forms::field-wrapper.label class="uk-form-label">SMS Notification (optional)</label>
+>>>>>>> e1b0bf9 (.)
                 <div class="uk-text-meta">Add a phone number to receive SMS notifications. Leave empty if you do not
                     wish to receive sms notifications</div>
             </div>
@@ -191,7 +233,11 @@ declare(strict_types=1);
         </div>
         <div class="uk-grid">
             <div class="uk-width-1-1@s uk-width-1-3@m">
+<<<<<<< HEAD
                 <label class="uk-form-label">Slack Notification (optional)</label>
+=======
+                <x-filament-forms::field-wrapper.label class="uk-form-label">Slack Notification (optional)</label>
+>>>>>>> e1b0bf9 (.)
                 <div class="uk-text-meta">Add a slack web hook url to recieve slack notifications. Leave empty if you
                     do not wish to receive slack notifications</div>
             </div>
@@ -218,7 +264,11 @@ declare(strict_types=1);
                 </ul>
             </div>
             <div class="uk-width-1-1@s uk-width-2-3@m uk-form-controls-text">
+<<<<<<< HEAD
                 <label class="uk-margin">
+=======
+                <x-filament-forms::field-wrapper.label class="uk-margin">
+>>>>>>> e1b0bf9 (.)
                     <input type="hidden" name="dont_overlap" id="dont_overlap" value="0"
                         {{ old('dont_overlap', $task->dont_overlap) ? '' : 'checked' }}>
                     <input type="checkbox" name="dont_overlap" id="dont_overlap" value="1"
@@ -227,7 +277,11 @@ declare(strict_types=1);
                 </label>
 
                 <div class="uk-margin">
+<<<<<<< HEAD
                     <label class="uk-margin">
+=======
+                    <x-filament-forms::field-wrapper.label class="uk-margin">
+>>>>>>> e1b0bf9 (.)
                         <input type="hidden" name="run_in_maintenance" id="run_in_maintenance" value="0"
                             {{ old('run_in_maintenance', $task->run_in_maintenance) ? '' : 'checked' }}>
                         <input type="checkbox" name="run_in_maintenance" id="run_in_maintenance" value="1"
@@ -236,7 +290,11 @@ declare(strict_types=1);
                     </label>
                 </div>
                 <div class="uk-margin">
+<<<<<<< HEAD
                     <label class="uk-margin">
+=======
+                    <x-filament-forms::field-wrapper.label class="uk-margin">
+>>>>>>> e1b0bf9 (.)
                         <input type="hidden" name="run_on_one_server" id="run_on_one_server" value="0"
                             {{ old('run_on_one_server', $task->run_on_one_server) ? '' : 'checked' }}>
                         <input type="checkbox" name="run_on_one_server" id="run_on_one_server" value="1"
@@ -245,7 +303,11 @@ declare(strict_types=1);
                     </label>
                 </div>
                 <div class="uk-margin">
+<<<<<<< HEAD
                     <label class="uk-margin">
+=======
+                    <x-filament-forms::field-wrapper.label class="uk-margin">
+>>>>>>> e1b0bf9 (.)
                         <input type="hidden" name="run_in_background" id="run_in_background" value="0"
                             {{ old('run_in_background', $task->run_in_background) ? '' : 'checked' }}>
                         <input type="checkbox" name="run_in_background" id="run_in_background" value="1"
@@ -265,18 +327,30 @@ declare(strict_types=1);
                 </ul>
             </div>
             <div class="uk-width-1-1@s uk-width-2-3@m uk-form-controls-text">
+<<<<<<< HEAD
                 <label class="uk-margin">
+=======
+                <x-filament-forms::field-wrapper.label class="uk-margin">
+>>>>>>> e1b0bf9 (.)
                     Auto Cleanup results after
                     <br>
                     <input class="uk-input" type="number" name="auto_cleanup_num" id="auto_cleanup_num"
                         value="{{ old('auto_cleanup_num', $task->auto_cleanup_num) ?? 0 }}" />
                     <br>
+<<<<<<< HEAD
                     <label>
+=======
+                    <x-filament-forms::field-wrapper.label>
+>>>>>>> e1b0bf9 (.)
                         <input type="radio" name="auto_cleanup_type" value="days"
                             {{ old('auto_cleanup_type', $task->auto_cleanup_type) !== 'results' ? 'checked' : '' }}>
                         Days
                     </label><br>
+<<<<<<< HEAD
                     <label>
+=======
+                    <x-filament-forms::field-wrapper.label>
+>>>>>>> e1b0bf9 (.)
                         <input type="radio" name="auto_cleanup_type" value="results"
                             {{ old('auto_cleanup_type', $task->auto_cleanup_type) === 'results' ? 'checked' : '' }}>
                         Results

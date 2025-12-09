ef80c4ec942a95cf010f506b77f77a7d810a1557
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:36:46 +0100

    .

--- app/Filament/Resources/ImportResource/Pages/ListImports.php
+++ app/Filament/Resources/ImportResource/Pages/ListImports.php
@@ -24,9 +24,6 @@ class ListImports extends XotBaseListRecords
      * @return array<string, Tables\Columns\Column>
      */
     #[Override]
-    /**
-     * @return array<string, mixed>
-     */
     public function getTableColumns(): array
     {
         return [
@@ -56,9 +53,6 @@ class ListImports extends XotBaseListRecords
      * @return array<string, BaseFilter>
      */
     #[Override]
-    /**
-     * @return array<string, mixed>
-     */
     public function getTableFilters(): array
     {
         return [];
@@ -68,9 +62,6 @@ class ListImports extends XotBaseListRecords
      * @return array<string, Action|ActionGroup>
      */
     #[Override]
-    /**
-     * @return array<string, mixed>
-     */
     public function getTableActions(): array
     {
         return [
@@ -82,9 +73,6 @@ class ListImports extends XotBaseListRecords
      * @return array<string, BulkAction>
      */
     #[Override]
-    /**
-     * @return array<string, mixed>
-     */
     public function getTableBulkActions(): array
     {
         return [

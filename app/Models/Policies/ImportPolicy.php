2fabaf641ac1010867f6aeee5f812f9dc8c0caf9
marco76tv <marco.sottana@gmail.com>
2025-12-09 23:54:32 +0100

    .

--- app/Models/Policies/ImportPolicy.php
+++ app/Models/Policies/ImportPolicy.php
@@ -5,63 +5,249 @@ declare(strict_types=1);
 namespace Modules\Job\Models\Policies;
 
 use Modules\Job\Models\Import;
+<<<<<<< HEAD
 use Modules\Xot\Contracts\UserContract;
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+use Modules\Xot\Contracts\UserContract;
+=======
+use Modules\Xot\Contracts\ProfileContract;
+>>>>>>> a12f125f4a (.)
+=======
+use Modules\Xot\Contracts\UserContract;
+>>>>>>> b93ef594b4 (.)
+=======
+use Modules\Xot\Contracts\UserContract;
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
 
 class ImportPolicy extends JobBasePolicy
 {
     /**
      * Determine whether the user can view any models.
      */
+<<<<<<< HEAD
     public function viewAny(UserContract $user): bool
     {
         return $user->hasPermissionTo('import.viewAny');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function viewAny(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('import.viewAny');
+=======
+    public function viewAny(ProfileContract $user): bool
+    {
+        return $user->hasPermissionTo('import.viewAny'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function viewAny(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('import.viewAny');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function viewAny(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('import.viewAny');
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can view the model.
      */
+<<<<<<< HEAD
     public function view(UserContract $user, Import $_import): bool
     {
         return $user->hasPermissionTo('import.view');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function view(UserContract $user, Import $_import): bool
+    {
+        return $user->hasPermissionTo('import.view');
+=======
+    public function view(ProfileContract $user, Import $import): bool
+    {
+        return $user->hasPermissionTo('import.view'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function view(UserContract $user, Import $_import): bool
+    {
+        return $user->hasPermissionTo('import.view');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function view(UserContract $user, Import $import): bool
+    {
+        return $user->hasPermissionTo('import.view');
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can create models.
      */
+<<<<<<< HEAD
     public function create(UserContract $user): bool
     {
         return $user->hasPermissionTo('import.create');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function create(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('import.create');
+=======
+    public function create(ProfileContract $user): bool
+    {
+        return $user->hasPermissionTo('import.create'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function create(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('import.create');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function create(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('import.create');
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can update the model.
      */
+<<<<<<< HEAD
     public function update(UserContract $user, Import $_import): bool
     {
         return $user->hasPermissionTo('import.update');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function update(UserContract $user, Import $_import): bool
+    {
+        return $user->hasPermissionTo('import.update');
+=======
+    public function update(ProfileContract $user, Import $import): bool
+    {
+        return $user->hasPermissionTo('import.update'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function update(UserContract $user, Import $_import): bool
+    {
+        return $user->hasPermissionTo('import.update');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function update(UserContract $user, Import $import): bool
+    {
+        return $user->hasPermissionTo('import.update');
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can delete the model.
      */
+<<<<<<< HEAD
     public function delete(UserContract $user, Import $_import): bool
     {
         return $user->hasPermissionTo('import.delete');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function delete(UserContract $user, Import $_import): bool
+    {
+        return $user->hasPermissionTo('import.delete');
+=======
+    public function delete(ProfileContract $user, Import $import): bool
+    {
+        return $user->hasPermissionTo('import.delete'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function delete(UserContract $user, Import $_import): bool
+    {
+        return $user->hasPermissionTo('import.delete');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function delete(UserContract $user, Import $import): bool
+    {
+        return $user->hasPermissionTo('import.delete');
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can restore the model.
      */
+<<<<<<< HEAD
     public function restore(UserContract $user, Import $_import): bool
     {
         return $user->hasPermissionTo('import.restore');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function restore(UserContract $user, Import $_import): bool
+    {
+        return $user->hasPermissionTo('import.restore');
+=======
+    public function restore(ProfileContract $user, Import $import): bool
+    {
+        return $user->hasPermissionTo('import.restore'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function restore(UserContract $user, Import $_import): bool
+    {
+        return $user->hasPermissionTo('import.restore');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function restore(UserContract $user, Import $import): bool
+    {
+        return $user->hasPermissionTo('import.restore');
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function forceDelete(UserContract $user, Import $import): bool
+    {
+        return $user->hasPermissionTo('import.forceDelete');
+=======
+    public function forceDelete(ProfileContract $user, Import $import): bool
+    {
+        return $user->hasPermissionTo('import.forceDelete'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function forceDelete(UserContract $user, Import $import): bool
+    {
+        return $user->hasPermissionTo('import.forceDelete');
+>>>>>>> b93ef594b4 (.)
+    }
+}
+=======
+>>>>>>> 548bbd3 (.)
     public function forceDelete(UserContract $user, Import $import): bool
     {
         return $user->hasPermissionTo('import.forceDelete');
     }
 }
+<<<<<<< HEAD
+=======
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)

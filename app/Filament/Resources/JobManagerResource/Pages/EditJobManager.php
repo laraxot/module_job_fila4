0fcb35bb12f7d821d5d0388cfd7af5fa2a301624
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:07:14 +0100

    .

--- app/Filament/Resources/JobManagerResource/Pages/EditJobManager.php
+++ app/Filament/Resources/JobManagerResource/Pages/EditJobManager.php
@@ -11,15 +11,7 @@ namespace Modules\Job\Filament\Resources\JobManagerResource\Pages;
 use Modules\Job\Filament\Resources\JobManagerResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
 
-<<<<<<< HEAD
 class EditJobManager extends XotBaseEditRecord
-=======
-<<<<<<< HEAD
-class EditJobManager extends XotBaseEditRecord
-=======
-class EditJobManager extends \Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
 {
     protected static string $resource = JobManagerResource::class;
 }

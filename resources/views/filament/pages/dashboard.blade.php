8ce751129dea2cf95964558232470ab5dbbba5f1
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:39:25 +0100

    .

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,11 +1,17 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 7d4742a (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
 =======
 >>>>>>> e1b0bf9 (.)
+=======
+>>>>>>> 7d4742a (.)
 <x-filament::page>
     {{--
     <img src="{{ ui::asset('ewall::img/logo.png') }}" />

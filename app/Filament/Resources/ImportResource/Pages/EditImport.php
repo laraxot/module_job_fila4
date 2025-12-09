e542a9414f8a2ea8f7d3de4529151001c5f48bcd
marco76tv <marco.sottana@gmail.com>
2025-12-09 23:49:36 +0100

    .

--- app/Filament/Resources/ImportResource/Pages/EditImport.php
+++ app/Filament/Resources/ImportResource/Pages/EditImport.php
@@ -7,15 +7,7 @@ namespace Modules\Job\Filament\Resources\ImportResource\Pages;
 use Modules\Job\Filament\Resources\ImportResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
 
-<<<<<<< HEAD
 class EditImport extends XotBaseEditRecord
-=======
-<<<<<<< HEAD
-class EditImport extends XotBaseEditRecord
-=======
-class EditImport extends \Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
 {
     protected static string $resource = ImportResource::class;
 }

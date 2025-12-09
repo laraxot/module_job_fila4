9d1ed00d912af00dce36f170715c851525ba6a7d
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:36:05 +0100

    .

--- app/Models/Export.php
+++ app/Models/Export.php
@@ -86,9 +86,12 @@ class Export extends BaseExport
             'created_by' => 'string',
             'deleted_by' => 'string',
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 >>>>>>> e1b0bf9 (.)
+=======
+>>>>>>> 7d4742a (.)
             'payload' => 'array',
             'created_at' => 'datetime',
             'updated_at' => 'datetime',

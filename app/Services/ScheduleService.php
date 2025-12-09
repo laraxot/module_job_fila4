ab5ebe8601903dba77bc7a505f1b4138079d7500
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:42:18 +0100

    .

--- app/Services/ScheduleService.php
+++ app/Services/ScheduleService.php
@@ -21,7 +21,9 @@ class ScheduleService
     public function __construct()
     {
         Assert::string($modelClass = config('job::model'), '['.__LINE__.']['.class_basename($this).']');
-        $this->model = app($modelClass);
+        $model = app($modelClass);
+        Assert::isInstanceOf($model, Schedule::class);
+        $this->model = $model;
     }
 
     /**

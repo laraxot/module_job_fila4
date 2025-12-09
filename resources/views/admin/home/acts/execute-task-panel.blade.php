f80a922f7429c612dcb15df830e7faa85d7fb819
marco76tv <marco.sottana@gmail.com>
2025-12-09 23:52:13 +0100

    .

--- resources/views/admin/home/acts/execute-task-panel.blade.php
+++ resources/views/admin/home/acts/execute-task-panel.blade.php
@@ -1,8 +1,26 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 548bbd3 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
 @extends('adm_theme::layouts.app')
 @section('content')
     <pre>

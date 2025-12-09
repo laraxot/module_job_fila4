87336c867d4724119db99e32946bbe98a4aacbb3
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:10:14 +0100

    .

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
+<<<<<<< HEAD
  * |--------------------------------------------------------------------------
  * | API Routes
  * |--------------------------------------------------------------------------
@@ -18,3 +19,17 @@ use Illuminate\Support\Facades\Route;
 /*
  * Route::middleware('auth:api')->get('/job', fn(Request $request) => $request->user());
  */
+=======
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+/*
+Route::middleware('auth:api')->get('/job', fn(Request $request) => $request->user());
+*/
+>>>>>>> e1b0bf9 (.)

544272190fd9086069abd2a67b83449926b94752
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:41:43 +0100

    .

--- lang/it/edit_failed_import_row.php
+++ lang/it/edit_failed_import_row.php
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 548bbd3 (.)
 <?php
 
 declare(strict_types=1);
@@ -9,3 +17,26 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> origin/develop
+<?php return array (
+  'actions' => 
+  array (
+    'delete' => 
+    array (
+      'label' => 'delete',
+    ),
+  ),
+<<<<<<< HEAD
+);
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+);
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)

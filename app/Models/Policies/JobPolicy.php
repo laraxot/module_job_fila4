2fadbb6df0e63bcd616f92cec3671131f79281b3
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:11:51 +0100

    .

--- app/Models/Policies/JobPolicy.php
+++ app/Models/Policies/JobPolicy.php
@@ -29,7 +29,11 @@ class JobPolicy extends UserBasePolicy
     /**
      * Determine whether the user can create models.
      */
+<<<<<<< HEAD
     public function create(UserContract $_user): bool
+=======
+    public function create(UserContract $user): bool
+>>>>>>> e1b0bf9 (.)
     {
         return true;
     }
@@ -38,7 +42,11 @@ class JobPolicy extends UserBasePolicy
      * Determine whether the user can update the model.
      */
     // public function update(UserContract $user, Team $team): bool
+<<<<<<< HEAD
     public function update(UserContract $_user): bool
+=======
+    public function update(UserContract $user): bool
+>>>>>>> e1b0bf9 (.)
     {
         // return $user->ownsTeam($team);
         return false;

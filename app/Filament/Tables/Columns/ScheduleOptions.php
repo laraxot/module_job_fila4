f7c89d15dbc0ae5e9ea1a363b7f0a26dbf6d5bc7
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:40:52 +0100

    .

--- app/Filament/Tables/Columns/ScheduleOptions.php
+++ app/Filament/Tables/Columns/ScheduleOptions.php
@@ -25,7 +25,9 @@ class ScheduleOptions extends TextColumn
 
         if ($this->withValue && \is_object($this->record) && method_exists($this->record, 'getOptions')) {
             /** @var array<int|string, string> $options */
-            return $this->record->getOptions();
+            $options = $this->record->getOptions();
+
+            return $options;
         }
 
         return [];

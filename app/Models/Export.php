435ae64249a0afe222eaa17cd1b3e2f32beb2be4
marco76tv <marco.sottana@gmail.com>
2025-12-09 23:50:15 +0100

    .

--- app/Models/Export.php
+++ app/Models/Export.php
@@ -59,6 +59,8 @@ use Illuminate\Support\Carbon;
  * @mixin IdeHelperExport
  * @mixin Eloquent
  * @mixin Eloquent
+ * @mixin IdeHelperExport
+ * @mixin Eloquent
  */
 class Export extends BaseExport
 {

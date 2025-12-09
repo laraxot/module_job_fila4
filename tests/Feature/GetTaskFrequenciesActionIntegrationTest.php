060b1426c40569b16d1a2cd002b44265634c489a
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:34:51 +0100

    .

--- tests/Feature/GetTaskFrequenciesActionIntegrationTest.php
+++ tests/Feature/GetTaskFrequenciesActionIntegrationTest.php
@@ -3,11 +3,10 @@
 declare(strict_types=1);
 
 use Modules\Job\Actions\GetTaskFrequenciesAction;
-use Modules\Job\Models\TaskFrequency;
 
 describe('GetTaskFrequenciesAction Integration', function () {
     beforeEach(function () {
-        $this->action = new GetTaskFrequenciesAction();
+        $this->action = new GetTaskFrequenciesAction;
     });
 
     it('returns array when config exists', function () {

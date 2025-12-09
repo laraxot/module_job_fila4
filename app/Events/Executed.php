43596e3aef2921ff97bf0e380a78f3cc666c0006
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:34:18 +0100

    .

--- app/Events/Executed.php
+++ app/Events/Executed.php
@@ -20,25 +20,10 @@ class Executed extends BroadcastingEvent
 
         $time_elapsed_secs = microtime(true) - $started;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7d4742a (.)
         $task->results()->create([
             'duration' => $time_elapsed_secs * 1000,
             'result' => $output,
         ]);
-<<<<<<< HEAD
-=======
-        $task->results()->create(
-            [
-                'duration' => $time_elapsed_secs * 1000,
-                'result' => $output,
-            ]
-        );
->>>>>>> e1b0bf9 (.)
-=======
->>>>>>> 7d4742a (.)
 
         $task->notify(new TaskCompleted($output));
         $task->autoCleanup();

cb93592d01832c67b605f1a9a1d8b50f76b46cd4
marco76tv <marco.sottana@gmail.com>
2025-12-09 23:48:31 +0100

    .

--- app/Filament/Pages/JobMonitor.php
+++ app/Filament/Pages/JobMonitor.php
@@ -11,9 +11,21 @@ class JobMonitor extends Page
 {
     use NavigationLabelTrait;
 
+<<<<<<< HEAD
     protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-computer-desktop';
 
     protected string $view = 'job::filament.pages.job-monitor';
+=======
+<<<<<<< HEAD
+    protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-computer-desktop';
+
+    protected string $view = 'job::filament.pages.job-monitor';
+=======
+    protected static ?string $navigationIcon = 'heroicon-o-computer-desktop';
+
+    protected static string $view = 'job::filament.pages.job-monitor';
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
 
     // public function mount(): void {
     //     $user = auth()->user();

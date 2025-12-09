d696f7e5929d9b263af480b20b1684f909eb2bbb
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:41:45 +0100

    .

--- app/Models/Policies/ScheduleHistoryPolicy.php
+++ app/Models/Policies/ScheduleHistoryPolicy.php
@@ -5,249 +5,63 @@ declare(strict_types=1);
 namespace Modules\Job\Models\Policies;
 
 use Modules\Job\Models\ScheduleHistory;
-<<<<<<< HEAD
 use Modules\Xot\Contracts\UserContract;
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-use Modules\Xot\Contracts\UserContract;
-=======
-use Modules\Xot\Contracts\ProfileContract;
->>>>>>> a12f125f4a (.)
-=======
-use Modules\Xot\Contracts\UserContract;
->>>>>>> b93ef594b4 (.)
-=======
-use Modules\Xot\Contracts\UserContract;
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
 
 class ScheduleHistoryPolicy extends JobBasePolicy
 {
     /**
      * Determine whether the user can view any models.
      */
-<<<<<<< HEAD
     public function viewAny(UserContract $user): bool
     {
         return $user->hasPermissionTo('schedule_history.viewAny');
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public function viewAny(UserContract $user): bool
-    {
-        return $user->hasPermissionTo('schedule_history.viewAny');
-=======
-    public function viewAny(ProfileContract $user): bool
-    {
-        return $user->hasPermissionTo('schedule_history.viewAny'); /** @phpstan-ignore method.nonObject */
->>>>>>> a12f125f4a (.)
-=======
-    public function viewAny(UserContract $user): bool
-    {
-        return $user->hasPermissionTo('schedule_history.viewAny');
->>>>>>> b93ef594b4 (.)
-=======
-    public function viewAny(UserContract $user): bool
-    {
-        return $user->hasPermissionTo('schedule_history.viewAny');
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can view the model.
      */
-<<<<<<< HEAD
     public function view(UserContract $user, ScheduleHistory $_schedule_history): bool
     {
         return $user->hasPermissionTo('schedule_history.view');
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public function view(UserContract $user, ScheduleHistory $_schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.view');
-=======
-    public function view(ProfileContract $user, ScheduleHistory $schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.view'); /** @phpstan-ignore method.nonObject */
->>>>>>> a12f125f4a (.)
-=======
-    public function view(UserContract $user, ScheduleHistory $_schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.view');
->>>>>>> b93ef594b4 (.)
-=======
-    public function view(UserContract $user, ScheduleHistory $schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.view');
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can create models.
      */
-<<<<<<< HEAD
     public function create(UserContract $user): bool
     {
         return $user->hasPermissionTo('schedule_history.create');
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public function create(UserContract $user): bool
-    {
-        return $user->hasPermissionTo('schedule_history.create');
-=======
-    public function create(ProfileContract $user): bool
-    {
-        return $user->hasPermissionTo('schedule_history.create'); /** @phpstan-ignore method.nonObject */
->>>>>>> a12f125f4a (.)
-=======
-    public function create(UserContract $user): bool
-    {
-        return $user->hasPermissionTo('schedule_history.create');
->>>>>>> b93ef594b4 (.)
-=======
-    public function create(UserContract $user): bool
-    {
-        return $user->hasPermissionTo('schedule_history.create');
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can update the model.
      */
-<<<<<<< HEAD
     public function update(UserContract $user, ScheduleHistory $_schedule_history): bool
     {
         return $user->hasPermissionTo('schedule_history.update');
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public function update(UserContract $user, ScheduleHistory $_schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.update');
-=======
-    public function update(ProfileContract $user, ScheduleHistory $schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.update'); /** @phpstan-ignore method.nonObject */
->>>>>>> a12f125f4a (.)
-=======
-    public function update(UserContract $user, ScheduleHistory $_schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.update');
->>>>>>> b93ef594b4 (.)
-=======
-    public function update(UserContract $user, ScheduleHistory $schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.update');
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-<<<<<<< HEAD
     public function delete(UserContract $user, ScheduleHistory $_schedule_history): bool
     {
         return $user->hasPermissionTo('schedule_history.delete');
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public function delete(UserContract $user, ScheduleHistory $_schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.delete');
-=======
-    public function delete(ProfileContract $user, ScheduleHistory $schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.delete'); /** @phpstan-ignore method.nonObject */
->>>>>>> a12f125f4a (.)
-=======
-    public function delete(UserContract $user, ScheduleHistory $_schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.delete');
->>>>>>> b93ef594b4 (.)
-=======
-    public function delete(UserContract $user, ScheduleHistory $schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.delete');
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-<<<<<<< HEAD
     public function restore(UserContract $user, ScheduleHistory $_schedule_history): bool
     {
         return $user->hasPermissionTo('schedule_history.restore');
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public function restore(UserContract $user, ScheduleHistory $_schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.restore');
-=======
-    public function restore(ProfileContract $user, ScheduleHistory $schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.restore'); /** @phpstan-ignore method.nonObject */
->>>>>>> a12f125f4a (.)
-=======
-    public function restore(UserContract $user, ScheduleHistory $_schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.restore');
->>>>>>> b93ef594b4 (.)
-=======
-    public function restore(UserContract $user, ScheduleHistory $schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.restore');
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public function forceDelete(UserContract $user, ScheduleHistory $schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.forceDelete');
-=======
-    public function forceDelete(ProfileContract $user, ScheduleHistory $schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.forceDelete'); /** @phpstan-ignore method.nonObject */
->>>>>>> a12f125f4a (.)
-=======
-    public function forceDelete(UserContract $user, ScheduleHistory $schedule_history): bool
-    {
-        return $user->hasPermissionTo('schedule_history.forceDelete');
->>>>>>> b93ef594b4 (.)
-    }
-}
-=======
->>>>>>> 548bbd3 (.)
     public function forceDelete(UserContract $user, ScheduleHistory $schedule_history): bool
     {
         return $user->hasPermissionTo('schedule_history.forceDelete');
     }
 }
-<<<<<<< HEAD
-=======
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)

45ef8f777d6e3ca10273aefbdcff1f0b75ad654a
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:29:50 +0100

    .

--- resources/views/components/repeater-table.blade.php
+++ resources/views/components/repeater-table.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7d4742a (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> e1b0bf9 (.)
-=======
->>>>>>> 7d4742a (.)
 <x-dynamic-component :component="$getFieldWrapperView()" :id="$getId()" :label="$getLabel()" :label-sr-only="$isLabelHidden()" :helper-text="$getHelperText()"
     :hint="$getHint()" :hint-icon="$getHintIcon()" :required="$isRequired()" :state-path="$getStatePath()">
     @php

ca11cc0a4e9c1fa04040e5959ad4a12d0b0bebe0
marco76tv <marco.sottana@gmail.com>
2025-12-09 23:51:21 +0100

    .

--- app/Models/Traits/FrontendSortable.php
+++ app/Models/Traits/FrontendSortable.php
@@ -13,15 +13,21 @@ trait FrontendSortable
      * @param  array<string>  $sortableColumns
      */
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 7d4742a (.)
     public function scopeSortableBy(
         Builder $query,
         array $sortableColumns,
         array $defaultSort = ['name' => 'asc'],
     ): Builder {
+<<<<<<< HEAD
 =======
     public function scopeSortableBy(Builder $query, array $sortableColumns, array $defaultSort = ['name' => 'asc']): Builder
     {
 >>>>>>> e1b0bf9 (.)
+=======
+>>>>>>> 7d4742a (.)
         $request = request();
         $sorted = $request->has('sort_by') && in_array($request->input('sort_by'), $sortableColumns, false);
 
@@ -38,6 +44,7 @@ trait FrontendSortable
             $sorted,
             static function (Builder $query) use ($sortByRequest, $sortDirectionRequest): void {
 <<<<<<< HEAD
+<<<<<<< HEAD
                 $query->orderBy((string) $sortByRequest, ((string) $sortDirectionRequest) === 'desc' ? 'desc' : 'asc');
 =======
                 $query->orderBy(
@@ -45,16 +52,23 @@ trait FrontendSortable
                     (string) $sortDirectionRequest === 'desc' ? 'desc' : 'asc'
                 );
 >>>>>>> e1b0bf9 (.)
+=======
+                $query->orderBy((string) $sortByRequest, ((string) $sortDirectionRequest) === 'desc' ? 'desc' : 'asc');
+>>>>>>> 7d4742a (.)
             },
             static function (Builder $query) use ($defaultSort): void {
                 foreach ($defaultSort as $key => $direction) {
                     $query->orderBy($key, $direction);
                 }
 <<<<<<< HEAD
+<<<<<<< HEAD
             },
 =======
             }
 >>>>>>> e1b0bf9 (.)
+=======
+            },
+>>>>>>> 7d4742a (.)
         );
     }
 }

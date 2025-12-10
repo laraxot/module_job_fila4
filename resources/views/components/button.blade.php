f8be01ac33e73b37d41b3a52962cb03562bf0ce2
marco76tv <marco.sottana@gmail.com>
2025-12-10 09:27:47 +0100

    .

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 548bbd3 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
 <x-job::icon-button :attributes="\Filament\Support\prepare_inherited_attributes($attributes)" :dark-mode="config('tables.dark_mode')">
     {{ $slot }}
 </x-job::icon-button>

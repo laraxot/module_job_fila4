ef966ca503542e6f940d02634dac72c11ac823df
marco76tv <marco.sottana@gmail.com>
2025-12-10 09:18:36 +0100

    .

--- database/factories/ScheduleHistoryFactory.php
+++ database/factories/ScheduleHistoryFactory.php
@@ -9,7 +9,15 @@ use Illuminate\Database\Eloquent\Model;
 use Modules\Job\Models\ScheduleHistory;
 
 /**
+<<<<<<< HEAD
  * @extends Factory<ScheduleHistory>
+=======
+<<<<<<< HEAD
+ * @extends Factory<ScheduleHistory>
+=======
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Modules\Job\Models\ScheduleHistory>
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
  */
 class ScheduleHistoryFactory extends Factory
 {
@@ -27,6 +35,24 @@ class ScheduleHistoryFactory extends Factory
      */
     public function definition()
     {
+<<<<<<< HEAD
         return [];
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        return [];
+=======
+        return [
+        ];
+>>>>>>> a12f125f4a (.)
+=======
+        return [];
+>>>>>>> b93ef594b4 (.)
+=======
+        return [
+        ];
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
     }
 }

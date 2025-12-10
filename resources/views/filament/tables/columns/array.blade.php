02f14b1543ddb2a64a8db6a237ecb716b16d7e54
marco76tv <marco.sottana@gmail.com>
2025-12-10 09:34:07 +0100

    .

--- resources/views/filament/tables/columns/array.blade.php
+++ resources/views/filament/tables/columns/array.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 548bbd3 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
 <div>
 
     <ul>
@@ -28,20 +10,4 @@ declare(strict_types=1);
         @include('job::filament.tables.columns.array.item',['key'=>$key,'value'=>$value])
     @endforeach
     </ul>
-<<<<<<< HEAD
 </div>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-</div>
-=======
-</div>
->>>>>>> a12f125f4a (.)
-=======
-</div>
->>>>>>> b93ef594b4 (.)
-=======
-</div>
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)

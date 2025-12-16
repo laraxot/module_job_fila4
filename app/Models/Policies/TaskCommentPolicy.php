ae49ce0a1912e25282f1c78ea3dbb3e3ae7ec38a
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:33:50 +0100

    .

--- app/Models/Policies/TaskCommentPolicy.php
+++ app/Models/Policies/TaskCommentPolicy.php
@@ -6,16 +6,21 @@ namespace Modules\Job\Models\Policies;
 
 use Modules\Job\Models\TaskComment;
 <<<<<<< HEAD
+<<<<<<< HEAD
 use Modules\Xot\Contracts\UserContract;
 =======
 use Modules\Xot\Contracts\ProfileContract;
 >>>>>>> e1b0bf9 (.)
+=======
+use Modules\Xot\Contracts\UserContract;
+>>>>>>> 7d4742a (.)
 
 class TaskCommentPolicy extends JobBasePolicy
 {
     /**
      * Determine whether the user can view any models.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function viewAny(UserContract $user): bool
     {
@@ -25,11 +30,17 @@ class TaskCommentPolicy extends JobBasePolicy
     {
         return $user->hasPermissionTo('task_comment.viewAny'); /** @phpstan-ignore method.nonObject */
 >>>>>>> e1b0bf9 (.)
+=======
+    public function viewAny(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('task_comment.viewAny');
+>>>>>>> 7d4742a (.)
     }
 
     /**
      * Determine whether the user can view the model.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function view(UserContract $user, TaskComment $_task_comment): bool
     {
@@ -39,11 +50,17 @@ class TaskCommentPolicy extends JobBasePolicy
     {
         return $user->hasPermissionTo('task_comment.view'); /** @phpstan-ignore method.nonObject */
 >>>>>>> e1b0bf9 (.)
+=======
+    public function view(UserContract $user, TaskComment $_task_comment): bool
+    {
+        return $user->hasPermissionTo('task_comment.view');
+>>>>>>> 7d4742a (.)
     }
 
     /**
      * Determine whether the user can create models.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function create(UserContract $user): bool
     {
@@ -53,11 +70,17 @@ class TaskCommentPolicy extends JobBasePolicy
     {
         return $user->hasPermissionTo('task_comment.create'); /** @phpstan-ignore method.nonObject */
 >>>>>>> e1b0bf9 (.)
+=======
+    public function create(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('task_comment.create');
+>>>>>>> 7d4742a (.)
     }
 
     /**
      * Determine whether the user can update the model.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function update(UserContract $user, TaskComment $_task_comment): bool
     {
@@ -67,11 +90,17 @@ class TaskCommentPolicy extends JobBasePolicy
     {
         return $user->hasPermissionTo('task_comment.update'); /** @phpstan-ignore method.nonObject */
 >>>>>>> e1b0bf9 (.)
+=======
+    public function update(UserContract $user, TaskComment $_task_comment): bool
+    {
+        return $user->hasPermissionTo('task_comment.update');
+>>>>>>> 7d4742a (.)
     }
 
     /**
      * Determine whether the user can delete the model.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function delete(UserContract $user, TaskComment $_task_comment): bool
     {
@@ -81,11 +110,17 @@ class TaskCommentPolicy extends JobBasePolicy
     {
         return $user->hasPermissionTo('task_comment.delete'); /** @phpstan-ignore method.nonObject */
 >>>>>>> e1b0bf9 (.)
+=======
+    public function delete(UserContract $user, TaskComment $_task_comment): bool
+    {
+        return $user->hasPermissionTo('task_comment.delete');
+>>>>>>> 7d4742a (.)
     }
 
     /**
      * Determine whether the user can restore the model.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function restore(UserContract $user, TaskComment $_task_comment): bool
     {
@@ -95,11 +130,17 @@ class TaskCommentPolicy extends JobBasePolicy
     {
         return $user->hasPermissionTo('task_comment.restore'); /** @phpstan-ignore method.nonObject */
 >>>>>>> e1b0bf9 (.)
+=======
+    public function restore(UserContract $user, TaskComment $_task_comment): bool
+    {
+        return $user->hasPermissionTo('task_comment.restore');
+>>>>>>> 7d4742a (.)
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function forceDelete(UserContract $user, TaskComment $task_comment): bool
     {
@@ -109,5 +150,10 @@ class TaskCommentPolicy extends JobBasePolicy
     {
         return $user->hasPermissionTo('task_comment.forceDelete'); /** @phpstan-ignore method.nonObject */
 >>>>>>> e1b0bf9 (.)
+=======
+    public function forceDelete(UserContract $user, TaskComment $task_comment): bool
+    {
+        return $user->hasPermissionTo('task_comment.forceDelete');
+>>>>>>> 7d4742a (.)
     }
 }

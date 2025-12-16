cf54d50fa0682214040d922bce2a08286f5bf282
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:28:31 +0100

    .

--- app/Filament/Resources/ScheduleResource/Pages/EditSchedule.php
+++ app/Filament/Resources/ScheduleResource/Pages/EditSchedule.php
@@ -25,9 +25,6 @@ class EditSchedule extends XotBaseEditRecord
     protected static string $resource = ScheduleResource::class;
 
     #[Override]
-    /**
-     * @return array<string, mixed>
-     */
     public function getformSchema(): array
     {
         Assert::isArray($res = $this->getResource()::getFormSchema());

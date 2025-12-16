19c4fe951c813b12875148747e88b615b326ed8a
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:29:45 +0100

    .

--- resources/views/livewire/job/status.blade.php
+++ resources/views/livewire/job/status.blade.php
@@ -41,11 +41,7 @@ declare(strict_types=1);
             <pre>{!! $out !!}</pre>
         </p>
 
-<<<<<<< HEAD
         <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">conn</x-filament-forms::field-wrapper.label>
-=======
-        <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">conn</div>
->>>>>>> bccac65 (.)
         <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         wire.model.lazy="form_data.conn">
             <option selected>Choose a country</option>

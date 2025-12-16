295a8e211867d35ca3b99cbc1f12c8ed0c7400ea
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:49:01 +0100

    .

--- app/Filament/Columns/ScheduleOptions.php
+++ app/Filament/Columns/ScheduleOptions.php
@@ -23,14 +23,6 @@ class ScheduleOptions extends TextColumn
     public function getTags(): array
     {
         /*
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 548bbd3 (.)
          * if($this->record==null){
          * return [];
          * }
@@ -40,28 +32,6 @@ class ScheduleOptions extends TextColumn
          * return parent::getTags();
          * }
          */
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin/develop
-        if($this->record==null){
-            return [];
-        }
-        if($this->withValue)
-        return $this->record->getOptions();
-        else{
-            return parent::getTags();
-        }
-        */
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
         return [];
     }
 }

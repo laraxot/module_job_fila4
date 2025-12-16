361033d03404ed3b8ac923e10195b11b9c73b5e7
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:40:04 +0100

    .

--- app/Models/Policies/FailedImportRowPolicy.php
+++ app/Models/Policies/FailedImportRowPolicy.php
@@ -5,63 +5,249 @@ declare(strict_types=1);
 namespace Modules\Job\Models\Policies;
 
 use Modules\Job\Models\FailedImportRow;
+<<<<<<< HEAD
 use Modules\Xot\Contracts\UserContract;
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+use Modules\Xot\Contracts\UserContract;
+=======
+use Modules\Xot\Contracts\ProfileContract;
+>>>>>>> a12f125f4a (.)
+=======
+use Modules\Xot\Contracts\UserContract;
+>>>>>>> b93ef594b4 (.)
+=======
+use Modules\Xot\Contracts\UserContract;
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
 
 class FailedImportRowPolicy extends JobBasePolicy
 {
     /**
      * Determine whether the user can view any models.
      */
+<<<<<<< HEAD
     public function viewAny(UserContract $user): bool
     {
         return $user->hasPermissionTo('failed_import_row.viewAny');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function viewAny(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.viewAny');
+=======
+    public function viewAny(ProfileContract $user): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.viewAny'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function viewAny(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.viewAny');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function viewAny(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.viewAny');
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can view the model.
      */
+<<<<<<< HEAD
     public function view(UserContract $user, FailedImportRow $_failed_import_row): bool
     {
         return $user->hasPermissionTo('failed_import_row.view');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function view(UserContract $user, FailedImportRow $_failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.view');
+=======
+    public function view(ProfileContract $user, FailedImportRow $failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.view'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function view(UserContract $user, FailedImportRow $_failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.view');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function view(UserContract $user, FailedImportRow $failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.view');
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can create models.
      */
+<<<<<<< HEAD
     public function create(UserContract $user): bool
     {
         return $user->hasPermissionTo('failed_import_row.create');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function create(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.create');
+=======
+    public function create(ProfileContract $user): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.create'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function create(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.create');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function create(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.create');
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can update the model.
      */
+<<<<<<< HEAD
     public function update(UserContract $user, FailedImportRow $_failed_import_row): bool
     {
         return $user->hasPermissionTo('failed_import_row.update');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function update(UserContract $user, FailedImportRow $_failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.update');
+=======
+    public function update(ProfileContract $user, FailedImportRow $failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.update'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function update(UserContract $user, FailedImportRow $_failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.update');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function update(UserContract $user, FailedImportRow $failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.update');
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can delete the model.
      */
+<<<<<<< HEAD
     public function delete(UserContract $user, FailedImportRow $_failed_import_row): bool
     {
         return $user->hasPermissionTo('failed_import_row.delete');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function delete(UserContract $user, FailedImportRow $_failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.delete');
+=======
+    public function delete(ProfileContract $user, FailedImportRow $failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.delete'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function delete(UserContract $user, FailedImportRow $_failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.delete');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function delete(UserContract $user, FailedImportRow $failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.delete');
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can restore the model.
      */
+<<<<<<< HEAD
     public function restore(UserContract $user, FailedImportRow $_failed_import_row): bool
     {
         return $user->hasPermissionTo('failed_import_row.restore');
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function restore(UserContract $user, FailedImportRow $_failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.restore');
+=======
+    public function restore(ProfileContract $user, FailedImportRow $failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.restore'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function restore(UserContract $user, FailedImportRow $_failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.restore');
+>>>>>>> b93ef594b4 (.)
+=======
+    public function restore(UserContract $user, FailedImportRow $failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.restore');
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    public function forceDelete(UserContract $user, FailedImportRow $failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.forceDelete');
+=======
+    public function forceDelete(ProfileContract $user, FailedImportRow $failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.forceDelete'); /** @phpstan-ignore method.nonObject */
+>>>>>>> a12f125f4a (.)
+=======
+    public function forceDelete(UserContract $user, FailedImportRow $failed_import_row): bool
+    {
+        return $user->hasPermissionTo('failed_import_row.forceDelete');
+>>>>>>> b93ef594b4 (.)
+    }
+}
+=======
+>>>>>>> 548bbd3 (.)
     public function forceDelete(UserContract $user, FailedImportRow $failed_import_row): bool
     {
         return $user->hasPermissionTo('failed_import_row.forceDelete');
     }
 }
+<<<<<<< HEAD
+=======
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)

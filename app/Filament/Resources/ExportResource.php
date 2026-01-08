81252d1628cab6973accd006d6583193329dc41e
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:37:02 +0100

    .

--- app/Filament/Resources/ExportResource.php
+++ app/Filament/Resources/ExportResource.php
@@ -46,5 +46,6 @@ class ExportResource extends XotBaseResource
                 ->columnSpanFull(),
             'created_at' => DateTimePicker::make('created_at')->disabled(),
             'updated_at' => DateTimePicker::make('updated_at')->disabled(),
-        ]);}
+        ]);
+    }
 }

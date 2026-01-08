db8eeb3a1f21f23e0d83e4dd0a5fcf1a0ecd1f50
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:43:03 +0100

    .

--- app/Notifications/TaskCompleted.php
+++ app/Notifications/TaskCompleted.php
@@ -15,28 +15,13 @@ class TaskCompleted extends Notification implements ShouldQueue
     use Queueable;
 
     /**
-<<<<<<< HEAD
-=======
-     * The task output.
-     */
-    private readonly string $output;
-
-    /**
->>>>>>> bccac65 (.)
      * Create a new notification instance.
      *
      * @return void
      */
-<<<<<<< HEAD
     public function __construct(
         private readonly string $output,
     ) {}
-=======
-    public function __construct(string $output)
-    {
-        $this->output = $output;
-    }
->>>>>>> bccac65 (.)
 
     /**
      * Get the notification's delivery channels.

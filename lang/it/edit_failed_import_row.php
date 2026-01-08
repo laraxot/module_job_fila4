c8e8b89e8fa1d935e20d04673e0ddc3bca9ef563
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:35:08 +0100

    .

--- lang/it/edit_failed_import_row.php
+++ lang/it/edit_failed_import_row.php
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 7d4742a (.)
 <?php
 
 declare(strict_types=1);
@@ -12,17 +8,3 @@ declare(strict_types=1);
         ],
     ],
 ];
-<<<<<<< HEAD
-=======
-<?php return array (
-  'actions' => 
-  array (
-    'delete' => 
-    array (
-      'label' => 'delete',
-    ),
-  ),
-);
->>>>>>> e1b0bf9 (.)
-=======
->>>>>>> 7d4742a (.)

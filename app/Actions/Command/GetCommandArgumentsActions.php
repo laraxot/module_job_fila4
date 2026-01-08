bcdec39ab03c19ca5c4d53ccf662957ce937cbc3
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:50:03 +0100

    .

--- app/Actions/Command/GetCommandArgumentsActions.php
+++ app/Actions/Command/GetCommandArgumentsActions.php
@@ -11,12 +11,16 @@ class GetCommandArgumentsActions
 {
     use QueueableAction;
 
+    /**
+     * @return array<string, mixed>
+     */
     public function execute(Command $command): array
     {
         $arguments = [];
         foreach ($command->getDefinition()->getArguments() as $argument) {
-            $arguments[] = [
-                'name' => $argument->getName(),
+            $name = $argument->getName();
+            $arguments[$name] = [
+                'name' => $name,
                 'default' => $argument->getDefault(),
                 'required' => $argument->isRequired(),
             ];

515c575b07248746bcd4a290f6a62208d41755eb
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:41:18 +0100

    .

--- app/Models/Policies/TaskPolicy.php
+++ app/Models/Policies/TaskPolicy.php
@@ -6,16 +6,21 @@ namespace Modules\Job\Models\Policies;
 
 use Modules\Job\Models\Task;
 <<<<<<< HEAD
+<<<<<<< HEAD
 use Modules\Xot\Contracts\UserContract;
 =======
 use Modules\Xot\Contracts\ProfileContract;
 >>>>>>> e1b0bf9 (.)
+=======
+use Modules\Xot\Contracts\UserContract;
+>>>>>>> 7d4742a (.)
 
 class TaskPolicy extends JobBasePolicy
 {
     /**
      * Determine whether the user can view any models.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function viewAny(UserContract $user): bool
     {
@@ -25,11 +30,17 @@ class TaskPolicy extends JobBasePolicy
     {
         return $user->hasPermissionTo('task.viewAny'); /** @phpstan-ignore method.nonObject */
 >>>>>>> e1b0bf9 (.)
+=======
+    public function viewAny(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('task.viewAny');
+>>>>>>> 7d4742a (.)
     }
 
     /**
      * Determine whether the user can view the model.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function view(UserContract $user, Task $_task): bool
     {
@@ -39,11 +50,17 @@ class TaskPolicy extends JobBasePolicy
     {
         return $user->hasPermissionTo('task.view'); /** @phpstan-ignore method.nonObject */
 >>>>>>> e1b0bf9 (.)
+=======
+    public function view(UserContract $user, Task $_task): bool
+    {
+        return $user->hasPermissionTo('task.view');
+>>>>>>> 7d4742a (.)
     }
 
     /**
      * Determine whether the user can create models.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function create(UserContract $user): bool
     {
@@ -53,11 +70,17 @@ class TaskPolicy extends JobBasePolicy
     {
         return $user->hasPermissionTo('task.create'); /** @phpstan-ignore method.nonObject */
 >>>>>>> e1b0bf9 (.)
+=======
+    public function create(UserContract $user): bool
+    {
+        return $user->hasPermissionTo('task.create');
+>>>>>>> 7d4742a (.)
     }
 
     /**
      * Determine whether the user can update the model.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function update(UserContract $user, Task $_task): bool
     {
@@ -67,11 +90,17 @@ class TaskPolicy extends JobBasePolicy
     {
         return $user->hasPermissionTo('task.update'); /** @phpstan-ignore method.nonObject */
 >>>>>>> e1b0bf9 (.)
+=======
+    public function update(UserContract $user, Task $_task): bool
+    {
+        return $user->hasPermissionTo('task.update');
+>>>>>>> 7d4742a (.)
     }
 
     /**
      * Determine whether the user can delete the model.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function delete(UserContract $user, Task $_task): bool
     {
@@ -81,11 +110,17 @@ class TaskPolicy extends JobBasePolicy
     {
         return $user->hasPermissionTo('task.delete'); /** @phpstan-ignore method.nonObject */
 >>>>>>> e1b0bf9 (.)
+=======
+    public function delete(UserContract $user, Task $_task): bool
+    {
+        return $user->hasPermissionTo('task.delete');
+>>>>>>> 7d4742a (.)
     }
 
     /**
      * Determine whether the user can restore the model.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function restore(UserContract $user, Task $_task): bool
     {
@@ -95,11 +130,17 @@ class TaskPolicy extends JobBasePolicy
     {
         return $user->hasPermissionTo('task.restore'); /** @phpstan-ignore method.nonObject */
 >>>>>>> e1b0bf9 (.)
+=======
+    public function restore(UserContract $user, Task $_task): bool
+    {
+        return $user->hasPermissionTo('task.restore');
+>>>>>>> 7d4742a (.)
     }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
+<<<<<<< HEAD
 <<<<<<< HEAD
     public function forceDelete(UserContract $user, Task $task): bool
     {
@@ -109,5 +150,10 @@ class TaskPolicy extends JobBasePolicy
     {
         return $user->hasPermissionTo('task.forceDelete'); /** @phpstan-ignore method.nonObject */
 >>>>>>> e1b0bf9 (.)
+=======
+    public function forceDelete(UserContract $user, Task $task): bool
+    {
+        return $user->hasPermissionTo('task.forceDelete');
+>>>>>>> 7d4742a (.)
     }
 }

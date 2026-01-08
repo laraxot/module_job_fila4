2369ad1a561def0ebfd752d47ff07594779de0a4
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:48:10 +0100

    .

--- lang/lang/it/job.php
+++ lang/lang/it/job.php
@@ -1,9 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 548bbd3 (.)
 declare(strict_types=1);
 
 return [
@@ -276,41 +272,6 @@ return [
             'high' => 'Alta',
             'normal' => 'Normale',
             'low' => 'Bassa',
-<<<<<<< HEAD
-=======
-=======
-return [
-    'actions' => [
-        'create' => [
-            'label' => 'create',
-        ],
-    ],
-    'fields' => [
-        'edit' => [
-            'label' => 'edit',
-        ],
-        'payload' => [
-            'label' => 'payload',
-        ],
-        'id' => [
-            'label' => 'id',
-        ],
-        'queue' => [
-            'label' => 'queue',
-        ],
-        'attempts' => [
-            'label' => 'attempts',
-        ],
-        'reserved_at' => [
-            'label' => 'reserved_at',
-        ],
-        'available_at' => [
-            'label' => 'available_at',
-        ],
-        'created_at' => [
-            'label' => 'created_at',
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
         ],
     ],
 ];

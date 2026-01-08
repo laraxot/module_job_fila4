99c66c9db4906d36c1f285b6cb595ecf8564e2d5
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:49:33 +0100

    .

--- app/Actions/Command/GetCommandOptionsActions.php
+++ app/Actions/Command/GetCommandOptionsActions.php
@@ -19,30 +19,10 @@ class GetCommandOptionsActions
         $options = [
             'withValue' => [],
             'withoutValue' => [
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 548bbd3 (.)
                 'verbose',
                 'quiet',
                 'ansi',
                 'no-ansi',
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-                'verbose', 'quiet', 'ansi', 'no-ansi',
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-                'verbose', 'quiet', 'ansi', 'no-ansi',
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
             ],
         ];
         foreach ($command->getDefinition()->getOptions() as $option) {

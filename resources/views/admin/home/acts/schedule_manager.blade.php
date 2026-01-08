c82106aa04621afc97f255ab3bd752f7f77759ee
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:40:45 +0100

    .

--- resources/views/admin/home/acts/schedule_manager.blade.php
+++ resources/views/admin/home/acts/schedule_manager.blade.php
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 548bbd3 (.)
 <?php
 
 declare(strict_types=1);
@@ -7,3 +15,21 @@ declare(strict_types=1);
 @section('content')
 {!! $out !!}
 @endsection
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+@extends('adm_theme::layouts.app')
+@section('content')
+{!! $out !!}
+@endsection
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+@extends('adm_theme::layouts.app')
+@section('content')
+{!! $out !!}
+@endsection
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)

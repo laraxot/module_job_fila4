8254ce44995b52110c96815ad100335d70ad6e63
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:35:53 +0100

    .

--- resources/views/admin/dashboard/item.blade.php
+++ resources/views/admin/dashboard/item.blade.php
@@ -1,26 +1,6 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 548bbd3 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
 <!-- dashboard item -->
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<!-- dashboard item -->
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-<!-- dashboard item -->
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)

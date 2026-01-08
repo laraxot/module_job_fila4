9729f1865a6b360163c4764bbf64bdeab5e3235e
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:38:00 +0100

    .

--- resources/views/admin/acts/schedule_status.blade.php
+++ resources/views/admin/acts/schedule_status.blade.php
@@ -1,10 +1,44 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 548bbd3 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)
 @extends('adm_theme::layouts.app')
 @section('content')
     <br/><br/>
     <livewire:schedule.status ></livewire:schedule.status>
+<<<<<<< HEAD
 @endsection
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+@endsection
+=======
+@endsection
+>>>>>>> a12f125f4a (.)
+=======
+@endsection
+>>>>>>> b93ef594b4 (.)
+=======
+@endsection
+>>>>>>> origin/develop
+>>>>>>> 548bbd3 (.)

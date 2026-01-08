0b225f8481600787b63056c26f9888af10325267
marco76tv <marco.sottana@gmail.com>
2026-01-08 09:47:24 +0100

    .

--- app/Filament/Resources/FailedImportRowResource/Pages/EditFailedImportRow.php
+++ app/Filament/Resources/FailedImportRowResource/Pages/EditFailedImportRow.php
@@ -7,15 +7,7 @@ namespace Modules\Job\Filament\Resources\FailedImportRowResource\Pages;
 use Modules\Job\Filament\Resources\FailedImportRowResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
 
-<<<<<<< HEAD
 class EditFailedImportRow extends XotBaseEditRecord
-=======
-<<<<<<< HEAD
-class EditFailedImportRow extends XotBaseEditRecord
-=======
-class EditFailedImportRow extends \Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
 {
     protected static string $resource = FailedImportRowResource::class;
 }
--- resources/views/admin/home/acts/schedule_manager.blade.php
+++ resources/views/admin/home/acts/schedule_manager.blade.php
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 548bbd3 (.)
 <?php
 
 declare(strict_types=1);
@@ -15,21 +7,3 @@ declare(strict_types=1);
 @section('content')
 {!! $out !!}
 @endsection
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-@extends('adm_theme::layouts.app')
-@section('content')
-{!! $out !!}
-@endsection
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-@extends('adm_theme::layouts.app')
-@section('content')
-{!! $out !!}
-@endsection
->>>>>>> origin/develop
->>>>>>> 548bbd3 (.)
